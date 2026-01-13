6bd92b70c8e2b2579709ff13179f75bf4751045d
hansvancalster <hans.vancalster@inbo.be>
2026-01-13 16:20:42 +0100

    update run_pipeline

--- source/pipelines/run_pipeline.R
+++ source/pipelines/run_pipeline.R
@@ -7,6 +7,7 @@ Sys.setenv(TAR_PROJECT = "validation_sample")
 
 # check status
 tar_visnetwork(targets_only = TRUE, physics = TRUE)
+#tar_manifest() |> View()
 
 # check reason if any is outdated
 tar_sitrep() |> dplyr::filter(dplyr::if_any(.cols = !c(name)))
@@ -14,15 +15,15 @@ tar_sitrep() |> dplyr::filter(dplyr::if_any(.cols = !c(name)))
 # run the pipeline
 px <- tar_make(as_job = TRUE, use_crew = TRUE)
 
-# run with profiling
-results <- profvis::profvis(
-  targets::tar_make(
-    callr_function = NULL, # Do not run the pipeline behind a callr::r() process.
-    use_crew = FALSE, # Disable parallel computing with crew (optional)
-    as_job = FALSE # Do not run the pipeline in a Posit Workbench / RStudio background job.
-  )
-)
-print(results, aggregate = TRUE) # aggregate = TRUE is crucial for interpretable flame graphs.
+# # run with profiling
+# results <- profvis::profvis(
+#   targets::tar_make(
+#     callr_function = NULL, # Do not run the pipeline behind a callr::r() process.
+#     use_crew = FALSE, # Disable parallel computing with crew (optional)
+#     as_job = FALSE # Do not run the pipeline in a Posit Workbench / RStudio background job.
+#   )
+# )
+# print(results, aggregate = TRUE) # aggregate = TRUE is crucial for interpretable flame graphs.
 
 
 tar_progress_summary()
@@ -67,17 +68,21 @@ ml <- tar_read(maps)
 
 
 ml[[1]]
-terra::cats(ml[[1]])
-
-terra::plot(ml[[1]], colNA = "orange")
+my_cats <- terra::cats(ml[[1]])
+my_cats
+coltab <- my_cats[[1]] |> dplyr::select(value, red, green, blue)
+terra::coltab(ml[[1]]) <- coltab
+terra::plot(ml[[1]], colNA = "white")
 terra::values(ml[[1]], row = 5000, nrows = 1)
-terra::coltab(ml[[1]])
-terra::cats(ml[[1]])
 terra::datatype(ml[[1]])
-terra::NAflag(ml[[1]]) # not preserved!
+terra::NAflag(ml[[1]])
 ft <- terra::freq(ml[[3]])
 ft |>
   dplyr::mutate(prop = round(count / sum(count), 4))
+
+
+
+
 
 grts <- tar_read(fleagrts)
 grts
@@ -100,20 +105,27 @@ tms
 terra::plot(tms)
 terra::values(tms, row = 1, nrows = 1)
 
-
+tms_cats <- terra::cats(tms)
 terra::activeCat(tms) <- "stable"
-terra::plot(tms, colNA = "orange")
+terra::plot(tms, colNA = "white")
 
 targets::tar_read(lu_changecats)
 
 sg <- targets::tar_read(separate_grts)
 all(purrr::map(sg, ~inherits(.x, "SpatRaster")) |> unlist())
+sg[[1]]
+
 
 vs <- targets::tar_read(validation_sample)
-terra::vect(vs) |> sf::st_as_sf(crs = 31370) |>
+terra::vect(vs) |>
+  sf::st_as_sf(crs = 31370) |>
   sf::st_drop_geometry() |>
-  dplyr::count(grts_rank) |> dplyr::count(n)
-terra::vect(vs) |> sf::st_as_sf(crs = 31370) |>
+  dplyr::count(grts_rank, name = "times_selected") |>
+  dplyr::count(times_selected) |>
+  dplyr::mutate(prop = n/ sum(n))
+
+terra::vect(vs) |>
+  sf::st_as_sf(crs = 31370) |>
   sf::st_drop_geometry() |>
   dplyr::count(stratum_name, changecat) |>
   tidyr::pivot_wider(names_from = changecat, values_from = n) |>
@@ -121,15 +133,21 @@ terra::vect(vs) |> sf::st_as_sf(crs = 31370) |>
 
 vp <- targets::tar_read(validation_polygons)
 lapply(vp, nrow) |> unlist() |> sum()
-
+# =3248 + 824*2 + 48*3 of 3248 + 824 + 48 = 4120 unieke rastercellen
 
 library(ggplot2)
 library(sf)
 terra::vect(vs) |>
   st_as_sf() |>
+  dplyr::mutate(
+    value = stringr::str_extract(
+      stratum_name, "^bc_(.*)_changecat", group = 1
+    ) |> as.numeric()
+  ) |>
+  dplyr::inner_join(ct, by = "value") |>
   ggplot() +
   geom_sf(aes(colour  = changecat), alpha = 0.2) +
-  facet_wrap(~ stratum_name)
+  facet_wrap(~ stringr::str_wrap(label, 15))
 
 terra::vect(vs) |>
   st_as_sf() |>
@@ -168,11 +186,18 @@ mapview::mapview(terra::vect(lbg_101), alpha.regions = 0.2
 
 prelabeled <- tar_read(prelabeled_validation_polygons)
 
+# 102
 mapview::mapview(prelabeled$prelabeled_validation_polygons_b0bae0239048f770,
-                 zcol = "collabel_2022")
+                 zcol = "labels")
 
-prelabeled$prelabeled_validation_polygons_b0bae0239048f770 |>
-  sf::st_as_sf()
+# 106
+mapview::mapview(prelabeled$prelabeled_validation_polygons_c66dc9ac1a4d5ea7,
+                 zcol = "labels")
+
+# 200
+mapview::mapview(prelabeled$prelabeled_validation_polygons_0692faf9b384816b,
+                 zcol = "labels")
+
 
 
 ##################
@@ -191,7 +216,7 @@ test <- get_grb_by_row(
   polygons = tar_read(validation_polygons_6e7d3123e950eb4d)[1:2,]
 )
 targets::tar_load_globals()
-targets::tar_workspace("vp_water_settlements_bc312eaba6d3a034")
+targets::tar_workspace("vp_water_settlements_5319be99c3d05901")
 debugonce(combine_water_settlements)
 test <- combine_water_settlements(
   water = vp_water,
@@ -230,23 +255,56 @@ test <- get_watersurfaces(
 )
 
 
-targets::tar_load_globals()
-targets::tar_workspace("prelabeled_validation_polygons_e9e42187a231237d")
-debugonce(intersect_validation_polygons)
-test <- intersect_validation_polygons(
- vp_water_settlements_singletarget, lu_changecats
+targets::tar_workspace("prelabeled_validation_polygons_50_04ec7c30fc6c9565")
+debugonce(crop_labeled_polygons)
+debug(combine_small_intersections)
+test <- crop_labeled_polygons(
+  pvp = prelabeled_validation_polygons,
+  crop_with = validation_polygons_50
 )
 
-targets::tar_workspace("vp_water_settlements_5319be99c3d05901")
+targets::tar_workspace("vp_water_grb_lbg_5319be99c3d05901")
 
-debugonce(combine_water_settlements)
+debugonce(combine_water_grb_lbg)
 
-result <- combine_water_settlements(
+result <- combine_water_grb_lbg(
   water = vp_water,
   settlements = grb_settlements_processed,
   lbg_101 = lbg_101_cropped,
   lbg_104 = lbg_104_cropped,
+  lbg_200 = lbg_200_cropped,
+  lbg_300 = lbg_300_cropped,
   polygons = validation_polygons
 )
 
+targets::tar_workspace(prelabeled_validation_polygons_6fdb9209e87c593c)
+debugonce(intersect_validation_polygons)
+debug(combine_small_intersections)
+result <- intersect_validation_polygons(
+  wsp_target = vp_water_grb_lbg_singletarget,
+  lu_changecat = lu_changecats,
+  input_years = input_years,
+  area_too_small = 10
+)
 
+hist(expanse(result))
+sum(expanse(result) < 10)
+
+targets::tar_workspace(vp_water_grb_lbg_5319be99c3d05901)
+debugonce(combine_water_grb_lbg)
+result <- combine_water_grb_lbg(
+  water = vp_water,
+  settlements = grb_settlements_processed,
+  lbg_list = list(!!!lbg_symbols),
+  polygons = validation_polygons
+)
+
+tar_manifest(fields = "command", names = vp_water_grb_lbg) |> View()
+
+targets::tar_workspace(raster_labeled_validation_polygons_50_de2d47ebdd431bdb)
+debugonce(raster_to_polygons)
+result <- raster_to_polygons(
+  tm = temporal_map_strata, # temporal raster
+  vp = validation_polygons_50, # validation polygons
+  input_years = as.character(input_years)
+)
